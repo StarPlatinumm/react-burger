93c1ac56e05616a7cc4cc9a32af36e4e5ade8f43
Кривдин Артем <akrivdin@data-platform.ru>
2023-08-15 20:49:08 +0300

    Скорректировал проверки Cypress

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -4,24 +4,27 @@ describe('Application', () => {
   beforeEach(() => {
     cy.intercept('GET', `${BURGER_API_URL}/ingredients`, { fixture: 'ingredients'});
     cy.visit('http://localhost:3000/');
+    cy.get("[data-test=ingredients]").contains('Просто булка').as('bun');
+    cy.get("[data-test=ingredients]").contains('Просто котлетка').as('main');
+    cy.get("[data-test=constructor]").as('constructor');
   })
 
   it("should open ingredients popup", () => {
-    cy.get("[data-test=ingredients]").contains('Просто булка').click();
+    cy.get("@bun").click();
     cy.get("[data-test=closeModalButton]").click();
   });
 
   it("should drag buns", () => {
-    cy.get("[data-test=ingredients]").contains('Просто булка').trigger("dragstart");
-    cy.get("[data-test=constructor]").trigger("drop");
+    cy.get("@bun").trigger("dragstart");
+    cy.get("@constructor").trigger("drop");
     cy.get("[data-test=top_bun]").contains('Просто булка (верх)').should("exist");
     cy.get("[data-test=bottom_bun]").contains('Просто булка (низ)').should("exist");
   });
 
   it("should drag mains", () => {
-    cy.get("[data-test=ingredients]").contains('Просто котлетка').trigger("dragstart");
-    cy.get("[data-test=constructor]").trigger("drop");
-    cy.get("[data-test=constructor]").contains('Просто котлетка').should("exist");
+    cy.get("@main").trigger("dragstart");
+    cy.get("@constructor").trigger("drop");
+    cy.get("@constructor").contains('Просто котлетка').should("exist");
   });
 
 })
